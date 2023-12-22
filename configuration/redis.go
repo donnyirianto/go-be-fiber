516e6032e56dc2977354ff028a60cf0578503bdc
zaranggi <donnyirianto.anggriawan@gmail.com>
2023-12-22 07:19:29 +0700

    update

--- configuration/redis.go
+++ configuration/redis.go
@@ -11,6 +11,7 @@ import (
 
 func NewRedis(config Config) (*redis.Client, error) {
 	requiredParams := []string{"REDIS_HOST", "REDIS_PORT", "REDIS_PASSWORD", "REDIS_POOL_MAX_SIZE", "REDIS_POOL_MIN_IDLE_SIZE"}
+
 	for _, param := range requiredParams {
 		if config.GetString(param) == "" {
 			err := errors.New("Missing required configuration parameter: " + param)
@@ -24,22 +25,25 @@ func NewRedis(config Config) (*redis.Client, error) {
 	maxPoolSize := config.GetInt("REDIS_POOL_MAX_SIZE")
 	minIdlePoolSize := config.GetInt("REDIS_POOL_MIN_IDLE_SIZE")
 
-	redisStore := redis.NewClient(&redis.Options{
+	options := &redis.Options{
 		Addr:         host + ":" + port,
 		Password:     password,
 		PoolSize:     maxPoolSize,
 		MinIdleConns: minIdlePoolSize,
-	})
+	}
+
+	redisStore := redis.NewClient(options)
 	return redisStore, nil
 }
 
 func SetCache[T any](cacheManager *redis.Client, ctx context.Context, prefix string, key string, executeData func(context.Context, string) (T, error)) *T {
-	var data []byte
 	var object T
-	if err := cacheManager.Get(ctx, prefix+"_"+key).Scan(&data); err == nil {
+
+	cacheKey := prefix + "_" + key
+
+	if data, err := cacheManager.Get(ctx, cacheKey).Bytes(); err == nil {
 		err := json.Unmarshal(data, &object)
 		if err != nil {
-			// Log the error
 			exception.PanicLogging(err)
 		}
 		return &object
@@ -47,22 +51,20 @@ func SetCache[T any](cacheManager *redis.Client, ctx context.Context, prefix str
 
 	value, err := executeData(ctx, key)
 	if err != nil {
-		// Log the error
 		exception.PanicLogging(err)
 		return nil
 	}
 
 	cacheValue, err := json.Marshal(value)
 	if err != nil {
-		// Log the error
 		exception.PanicLogging(err)
 		return nil
 	}
 
-	if err := cacheManager.Set(ctx, prefix+"_"+key, cacheValue, -1).Err(); err != nil {
-		// Log the error
+	if err := cacheManager.Set(ctx, cacheKey, cacheValue, -1).Err(); err != nil {
 		exception.PanicLogging(err)
 		return nil
 	}
+
 	return &value
 }
